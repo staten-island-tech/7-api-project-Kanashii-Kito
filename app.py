df655fd57137aa818b14c08535ab7d52786adb1c
Kanashii-Kito <ERicc468@nycstudents.net>
2026-01-05 21:19:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
 """ https://anapioficeandfire.com/"""
 
-import tkinter as tk
+# import tkinter as tk
+# import requests
+
+# window = tk()
+# window.geometry("500x500")
+# window.title("test")
+
+# enteruser = Entry(window, font="Arial, 12")
+# submit_button = Button(window, text="Submit Data", font="Arial, 12", bg="yellow")
+# testlabel = Label(window, font="Arial, 12")
+
+
 import requests
 
-window = tk()
-window.geometry("500x500")
-window.title("test")
+def test(temptest):
+    url = f"https://anapioficeandfire.com/api/books/{temptest}"
+    response = requests.get(url)
 
-enteruser = Entry(window, font="Arial, 12")
-submit_button = Button(window, text="Submit Data", font="Arial, 12", bg="yellow")
-testlabel = Label(window, font="Arial, 12")
+    if response.status_code != 200:
+        print("Error fetching data!", response.status_code)
+        return None
+    
+    data = response.json()
+    return {
+        "name": data["name"],
+        "isbn": data["isbn"],
+        "authors": data["authors"],
+        "number_of_pages": data["numberOfPages"],
+        "publisher": data["publisher"],
+        "released": data["released"],
+        "characters": data["characters"],
+        "pov_characters": data["povCharacters"]
+    }
+
+testv1 = test(1)
+print(testv1)
+
